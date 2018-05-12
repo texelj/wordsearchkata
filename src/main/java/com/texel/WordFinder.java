3cb1783a4dded68fa735954d1c1e2ec9a3fc985b
Jacob <jacobtexel@gmail.com>
2018-05-12 17:19:11 -0400

    Word finder returns empty string when word not found.

---
+++ src/main/java/com/texel/WordFinder.java
@@ -0,0 +1,19 @@
+package com.texel;
+
+import java.util.List;
+
+/**
+ * Created by jacob on 5/12/2018.
+ */
+public class WordFinder {
+    private List<List<Character>> searchGrid;
+    public WordFinder(List<List<Character>> searchGrid) {
+        this.searchGrid=searchGrid;
+    }
+
+
+    public String findWord(String word) {
+
+        return "";
+    }
+}
---
+++ src/test/java/com/texel/WordFinderTest.java
@@ -0,0 +1,29 @@
+package com.texel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by jacob on 5/12/2018.
+ */
+public class WordFinderTest {
+    private WordFinder subject;
+    @Before
+    public void initialize() throws FileNotFoundException, URISyntaxException {
+        File testFile = new File(this.getClass().getResource("/alldirectionstest.txt").toURI());
+        WordSearchFileParser parser = new WordSearchFileParser();
+        WordSearchData data = parser.parseFile(testFile);
+        subject = new WordFinder(data.getSearchGrid());
+    }
+
+    @Test
+    public void FinderReturnsEmptyStringWhenWordNotFound(){
+        assertEquals(subject.findWord("NOTTHERE"),"");
+    }
+}
