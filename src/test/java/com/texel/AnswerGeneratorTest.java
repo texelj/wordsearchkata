440f759776b7adc2a635f28f2cc6c7a70e6b488a
Jacob <jacobtexel@gmail.com>
2018-05-12 18:23:35 -0400

    Added before block to answer generator test file.

--- src/test/java/com/texel/AnswerGeneratorTest.java
+++ src/test/java/com/texel/AnswerGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.texel;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -8,15 +9,20 @@ import static junit.framework.TestCase.assertEquals;
  * Created by jacob on 5/12/2018.
  */
 public class AnswerGeneratorTest {
+    private static AnswerGenerator subject;
+    @Before
+    public void initialize(){
+        subject = new AnswerGenerator();
+    }
 
     @Test
-    public void AnswerGeneratorReturnsSeparatorString(){
-        assertEquals(AnswerGenerator.generate(WordDirection.DIAGONAL_DESC,"",0,0),": ");
+    public void AnswerGeneratorReturnsSeparatorStringForBlankWord(){
+        assertEquals(subject.generate(WordDirection.DIAGONAL_DESC,"",0,0),": ");
     }
 
     @Test
     public void AnswerGeneratorReturnsAnswerStringForWord(){
-        assertEquals(AnswerGenerator.generate(WordDirection.DIAGONAL_ASC,"HELLO",4,0),"HELLO: (0,4),(1,3),(2,2),(3,1),(4,0)");
+        assertEquals(subject.generate(WordDirection.DIAGONAL_ASC,"HELLO",4,0),"HELLO: (0,4),(1,3),(2,2),(3,1),(4,0)");
     }
 
 }
