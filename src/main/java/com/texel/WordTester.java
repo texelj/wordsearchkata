f08b7e0ce00f60ff69e4f98494e30de48bf62de6
Jacob <jacobtexel@gmail.com>
2018-05-12 18:43:42 -0400

    Refactor: move boundary check outside of loop.

--- src/main/java/com/texel/WordTester.java
+++ src/main/java/com/texel/WordTester.java
@@ -13,9 +13,14 @@ public class WordTester {
     }
 
     public boolean test(List<List<Character>> searchGrid, String word, int row, int column) {
+        if(row<0 || row>=searchGrid.size() || column<0 || column>=searchGrid.size() ||
+                row+direction.rowDirection()*(word.length()-1)<0 ||
+                row+direction.rowDirection()*(word.length()-1)>=searchGrid.size() ||
+                column+direction.columnDirection()*(word.length()-1)<0 ||
+                column+direction.columnDirection()*(word.length()-1)>=searchGrid.size())
+            return false;
+
         for(int charIndex = 0; charIndex<word.length(); charIndex++){
-            if(row<0 || row>=searchGrid.size() || column<0 || column>=searchGrid.get(0).size())
-                return false;
             if(word.charAt(charIndex)!=searchGrid.get(row).get(column))
                 return false;
             column+=direction.columnDirection();
