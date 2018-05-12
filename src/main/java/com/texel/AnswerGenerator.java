3157c3cf4f084e780db9c4eb8ccb058c8b61c49d
Jacob <jacobtexel@gmail.com>
2018-05-12 17:05:06 -0400

    Test answer generator returns comma-separated list of coordinates for word.

--- src/main/java/com/texel/AnswerGenerator.java
+++ src/main/java/com/texel/AnswerGenerator.java
@@ -8,7 +8,13 @@ public class AnswerGenerator {
         StringBuffer answer = new StringBuffer();
         answer.append(word);
         answer.append(": ");
-
+        for(int i=0; i<word.length(); i++){
+            answer.append("(").append(column).append(",").append(row).append(")");
+            if(i<word.length()-1)
+                answer.append(",");
+            row+=direction.rowDirection();
+            column+=direction.columnDirection();
+        }
         answer.append("");
         return answer.toString();
     }
--- src/test/java/com/texel/AnswerGeneratorTest.java
+++ src/test/java/com/texel/AnswerGeneratorTest.java
@@ -10,8 +10,13 @@ import static junit.framework.TestCase.assertEquals;
 public class AnswerGeneratorTest {
 
     @Test
-    public void AnswerGeneratorReturnsString(){
+    public void AnswerGeneratorReturnsSeparatorString(){
         assertEquals(AnswerGenerator.generate(WordDirection.DIAGONAL_DESC,"",0,0),": ");
+    }
+
+    @Test
+    public void AnswerGeneratorReturnsAnswerStringForWord(){
+        assertEquals(AnswerGenerator.generate(WordDirection.DIAGONAL_ASC,"HELLO",4,0),"HELLO: (0,4),(1,3),(2,2),(3,1),(4,0)");
     }
 
 }
